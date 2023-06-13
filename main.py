2073af1bd29a390aa45eddd49de38c16a54a61b3
DamjanVincic <damjanvincic2003@gmail.com>
2023-06-13 22:50:44 +0200

    Added storing and loaded data using pickle

--- main.py
+++ main.py
@@ -1,13 +1,14 @@
 import csv
 import networkx as nx
 from datetime import datetime
+import pickle
 import parse_files
 from entities.status import Status
 from entities.comment import Comment
 from entities.share import Share
 from entities.reaction import Reaction
 
-
+users = {}
 statuses = {}
 shares = {}
 reactions = {}
@@ -76,31 +77,54 @@ def create_graph(data):
     return graph
 
 if __name__ == "__main__":
-    users = load_users("dataset/friends.csv")
-    for row in parse_files.load_statuses("dataset/original_statuses.csv"):
-        status = Status(row[0], row[1], row[2], row[3], datetime.strptime(row[4], "%Y-%m-%d %H:%M:%S"), row[5], row[6], row[7], row[8], row[9], row[10], row[11], row[12], row[13], row[14])
-        if status.author not in statuses:
-            statuses[status.author] = [status]
-        else:
-            statuses[status.author].append(status)
+    with open("entities.pickle", "rb") as f:
+        data = pickle.load(f)
+        users = data["users"]
+        statuses = data["statuses"]
+        shares = data["shares"]
+        reactions = data["shares"]
+        comments = data["comments"]
+    
 
-    for row in parse_files.load_shares("dataset/original_shares.csv"):
-        share = Share(row[0], row[1], datetime.strptime(row[2], "%Y-%m-%d %H:%M:%S"))
-        if share.status_id not in shares:
-            shares[share.status_id] = [share]
-        else:
-            shares[share.status_id].append(share)
+    with open("user_graph.pickle", "rb") as f:
+        graph = pickle.load(f)
+
+
+    # users = load_users("dataset/friends.csv")
+    # for row in parse_files.load_statuses("dataset/original_statuses.csv"):
+    #     status = Status(row[0], row[1], row[2], row[3], datetime.strptime(row[4], "%Y-%m-%d %H:%M:%S"), row[5], row[6], row[7], row[8], row[9], row[10], row[11], row[12], row[13], row[14])
+    #     if status.author not in statuses:
+    #         statuses[status.author] = [status]
+    #     else:
+    #         statuses[status.author].append(status)
+
+    # for row in parse_files.load_shares("dataset/original_shares.csv"):
+    #     share = Share(row[0], row[1], datetime.strptime(row[2], "%Y-%m-%d %H:%M:%S"))
+    #     if share.status_id not in shares:
+    #         shares[share.status_id] = [share]
+    #     else:
+    #         shares[share.status_id].append(share)
     
-    for row in parse_files.load_reactions("dataset/original_reactions.csv"):
-        reaction = Reaction(row[0], row[1], row[2], datetime.strptime(row[3], "%Y-%m-%d %H:%M:%S"))
-        if reaction.status_id not in reactions:
-            reactions[reaction.status_id] = [reaction]
-        else:
-            reactions[reaction.status_id].append(reaction)
+    # for row in parse_files.load_reactions("dataset/original_reactions.csv"):
+    #     reaction = Reaction(row[0], row[1], row[2], datetime.strptime(row[3], "%Y-%m-%d %H:%M:%S"))
+    #     if reaction.status_id not in reactions:
+    #         reactions[reaction.status_id] = [reaction]
+    #     else:
+    #         reactions[reaction.status_id].append(reaction)
     
-    for row in parse_files.load_comments("dataset/original_comments.csv"):
-        comment = Comment(row[0], row[1], row[2], row[3], row[4], datetime.strptime(row[5], "%Y-%m-%d %H:%M:%S"), row[6], row[7])
-        if comment.status_id not in comments:
-            comments[comment.status_id] = [comment]
-        else:
-            comments[comment.status_id].append(comment)
+    # for row in parse_files.load_comments("dataset/original_comments.csv"):
+    #     comment = Comment(row[0], row[1], row[2], row[3], row[4], datetime.strptime(row[5], "%Y-%m-%d %H:%M:%S"), row[6], row[7])
+    #     if comment.status_id not in comments:
+    #         comments[comment.status_id] = [comment]
+    #     else:
+    #         comments[comment.status_id].append(comment)
+
+    # graph = create_graph(users)
+    
+    
+    # with open("user_graph.pickle", 'wb') as f:
+    #     pickle.dump(graph, f)
+    
+    # dictionary = {"users": users, "statuses": statuses, "shares": shares, "reactions": reactions, "comments": comments}
+    # with open("entities.pickle", "wb") as f:
+    #     pickle.dump(dictionary, f)
