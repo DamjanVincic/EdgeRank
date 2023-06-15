b42ca461f3179035b44b9fa16213de301cd74617
DamjanVincic <damjanvincic2003@gmail.com>
2023-06-15 21:06:57 +0200

    Removed comments

--- main.py
+++ main.py
@@ -141,89 +141,6 @@ def print_status(status):
     print(f"Message: {status.message}\nAuthor: {status.author}\nPublish Time: {status.publish_time}\nReactions: {status.reaction_count}\nComments: {status.comment_count}\nShares: {status.share_count}\n\n------\n")
 
 if __name__ == "__main__":
-    # users, friends = load_users("dataset/friends.csv")
-
-    # for row in parse_files.load_statuses("dataset/original_statuses.csv"):
-    #     status = Status(row[0], row[1], row[2], row[3], datetime.strptime(row[4], "%Y-%m-%d %H:%M:%S"), row[5], int(row[6]), int(row[7]), int(row[8]), int(row[9]), int(row[10]), int(row[11]), int(row[12]), int(row[13]), int(row[14]))
-    #     if status.author not in statuses:
-    #         statuses[status.author] = [status]
-    #     else:
-    #         statuses[status.author].append(status)
-    # for row in parse_files.load_statuses("dataset/test_statuses.csv"):
-    #     status = Status(row[0], row[1], row[2], row[3], datetime.strptime(row[4], "%Y-%m-%d %H:%M:%S"), row[5], int(row[6]), int(row[7]), int(row[8]), int(row[9]), int(row[10]), int(row[11]), int(row[12]), int(row[13]), int(row[14]))
-    #     if status.author not in statuses:
-    #         statuses[status.author] = [status]
-    #     else:
-    #         statuses[status.author].append(status)
-
-
-    # for row in parse_files.load_shares("dataset/original_shares.csv"):
-    #     share = Share(row[0], row[1], datetime.strptime(row[2], "%Y-%m-%d %H:%M:%S"))
-    #     if share.status_id not in shares:
-    #         shares[share.status_id] = [share]
-    #     else:
-    #         shares[share.status_id].append(share)
-    # for row in parse_files.load_shares("dataset/test_shares.csv"):
-    #     share = Share(row[0], row[1], datetime.strptime(row[2], "%Y-%m-%d %H:%M:%S"))
-    #     if share.status_id not in shares:
-    #         shares[share.status_id] = [share]
-    #     else:
-    #         shares[share.status_id].append(share)
-    
-
-    # for row in parse_files.load_reactions("dataset/original_reactions.csv"):
-    #     reaction = Reaction(row[0], row[1], row[2], datetime.strptime(row[3], "%Y-%m-%d %H:%M:%S"))
-    #     if reaction.status_id not in reactions:
-    #         reactions[reaction.status_id] = [reaction]
-    #     else:
-    #         reactions[reaction.status_id].append(reaction)
-    # for row in parse_files.load_reactions("dataset/test_reactions.csv"):
-    #     reaction = Reaction(row[0], row[1], row[2], datetime.strptime(row[3], "%Y-%m-%d %H:%M:%S"))
-    #     if reaction.status_id not in reactions:
-    #         reactions[reaction.status_id] = [reaction]
-    #     else:
-    #         reactions[reaction.status_id].append(reaction)
-    
-
-    # for row in parse_files.load_comments("dataset/original_comments.csv"):
-    #     comment = Comment(row[0], row[1], row[2], row[3], row[4], datetime.strptime(row[5], "%Y-%m-%d %H:%M:%S"), int(row[6]), int(row[7]))
-    #     if comment.status_id not in comments:
-    #         comments[comment.status_id] = [comment]
-    #     else:
-    #         comments[comment.status_id].append(comment)
-    # for row in parse_files.load_comments("dataset/test_comments.csv"):
-    #     comment = Comment(row[0], row[1], row[2], row[3], row[4], datetime.strptime(row[5], "%Y-%m-%d %H:%M:%S"), int(row[6]), int(row[7]))
-    #     if comment.status_id not in comments:
-    #         comments[comment.status_id] = [comment]
-    #     else:
-    #         comments[comment.status_id].append(comment)
-
-    # with open("pickles/entities.pickle", "rb") as f:
-    #     data = pickle.load(f)
-    #     users = data["users"]
-    #     friends = data["friends"]
-
-    # dictionary = {"users": users, "friends": friends, "statuses": statuses, "shares": shares, "reactions": reactions, "comments": comments}
-    # with open("pickles/test_entities.pickle", "wb") as f:
-    #     pickle.dump(dictionary, f)
-
-    # trie = Trie(reduce(lambda x, y: x + y, statuses.values()))
-    # with open("pickles/test_trie.pickle", "wb") as f:
-    #     pickle.dump(trie, f)
-
-    # with open("pickles/trie.pickle", "rb") as f:
-    #     trie = pickle.load(f)
-
-
-    # with open("pickles/entities.pickle", "rb") as f:
-    #     data = pickle.load(f)
-    #     users = data["users"]
-    #     friends = data["friends"]
-    #     statuses = data["statuses"]
-    #     shares = data["shares"]
-    #     reactions = data["reactions"]
-    #     comments = data["comments"]
-
     with open("pickles/test_entities.pickle", "rb") as f:
         data = pickle.load(f)
         users = data["users"]
@@ -233,26 +150,13 @@ if __name__ == "__main__":
         reactions = data["reactions"]
         comments = data["comments"]
 
-    # graph = create_graph(users)
-    # with open("pickles/user_graph.pickle", "wb") as f:
-    #     pickle.dump(graph, f)
-
-    # with open("pickles/user_graph.pickle", "rb") as f:
-    #     graph = pickle.load(f)
-
     with open("pickles/friend_user_graph.pickle", "rb") as f:
         graph = pickle.load(f)
 
-    # graph = add_friend_affinities(graph)
-    # with open("pickles/friend_user_graph.pickle", "wb") as f:
-    #     pickle.dump(graph, f)
-
-    # with open("pickles/trie.pickle", "rb") as f:
-    #     trie = pickle.load(f)
     with open("pickles/test_trie.pickle", "rb") as f:
         trie = pickle.load(f)
-    # trie = Trie(reduce(lambda x, y: x + y, statuses.values()))
 
+    
     name = input("Enter a user's name: ").title()
     while name not in users:
         print("User with that name doesnt exist.")
@@ -265,9 +169,6 @@ if __name__ == "__main__":
             if choice == 1:
                 recommended_statuses = sorted(reduce(lambda x, y: x + y, statuses.values()), key = edgerank, reverse = True)
                 for status in recommended_statuses[:10]:
-                    # print(f"Affinity: {0 if graph.get_edge_data(name, status.author) is None else graph.get_edge_data(name, status.author)['weight']}, Weight: {calculate_status_weight(status)}, Edgerank: {edgerank(status)}, Time posted: {status.publish_time}")
-                    # print(tabulate([[f"{status.message[:150]}...", status.author]], headers = ["Message", "Author"], tablefmt="fancy_grid"))
-                    # print(format_status(status))
                     print_status(status)
             elif choice == 2:
                 query = input("Enter search: ").lower()
@@ -277,14 +178,12 @@ if __name__ == "__main__":
                     results = trie.search_exact_query(query[1:-1].lower())
                     results.sort(key = edgerank, reverse = True)
                     for result in results[:10]:
-                        # print(tabulate([[f"{result.message[:150]}...", result.author]], headers = ["Message", "Author"], tablefmt="fancy_grid"))
                         print_status(result)
                 else:
                     results = list(trie.search_query(query))
                     results.sort(key = lambda result: result[0] + edgerank(result[1]), reverse = True)
                     results = list(map(lambda x: x[1], results))
                     for result in results[:10]:
-                        # print(tabulate([[f"{result.message[:150]}...", result.author]], headers = ["Message", "Author"], tablefmt="fancy_grid"))
                         print_status(result)
             elif choice == 3:
                 break
@@ -292,10 +191,3 @@ if __name__ == "__main__":
                 raise Exception
         except:
             pass
-
-    # with open("pickles/user_graph.pickle", 'wb') as f:
-    #     pickle.dump(graph, f)
-    
-    # dictionary = {"users": users, "friends": friends, "statuses": statuses, "shares": shares, "reactions": reactions, "comments": comments}
-    # with open("pickles/entities.pickle", "wb") as f:
-    #     pickle.dump(dictionary, f)
