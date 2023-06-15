afeacb96ef6d456db81314655bd2d53b8974efdb
DamjanVincic <damjanvincic2003@gmail.com>
2023-06-15 21:04:09 +0200

    Added test data

--- main.py
+++ main.py
@@ -149,6 +149,13 @@ if __name__ == "__main__":
     #         statuses[status.author] = [status]
     #     else:
     #         statuses[status.author].append(status)
+    # for row in parse_files.load_statuses("dataset/test_statuses.csv"):
+    #     status = Status(row[0], row[1], row[2], row[3], datetime.strptime(row[4], "%Y-%m-%d %H:%M:%S"), row[5], int(row[6]), int(row[7]), int(row[8]), int(row[9]), int(row[10]), int(row[11]), int(row[12]), int(row[13]), int(row[14]))
+    #     if status.author not in statuses:
+    #         statuses[status.author] = [status]
+    #     else:
+    #         statuses[status.author].append(status)
+
 
     # for row in parse_files.load_shares("dataset/original_shares.csv"):
     #     share = Share(row[0], row[1], datetime.strptime(row[2], "%Y-%m-%d %H:%M:%S"))
@@ -156,27 +163,68 @@ if __name__ == "__main__":
     #         shares[share.status_id] = [share]
     #     else:
     #         shares[share.status_id].append(share)
+    # for row in parse_files.load_shares("dataset/test_shares.csv"):
+    #     share = Share(row[0], row[1], datetime.strptime(row[2], "%Y-%m-%d %H:%M:%S"))
+    #     if share.status_id not in shares:
+    #         shares[share.status_id] = [share]
+    #     else:
+    #         shares[share.status_id].append(share)
     
+
     # for row in parse_files.load_reactions("dataset/original_reactions.csv"):
     #     reaction = Reaction(row[0], row[1], row[2], datetime.strptime(row[3], "%Y-%m-%d %H:%M:%S"))
     #     if reaction.status_id not in reactions:
     #         reactions[reaction.status_id] = [reaction]
     #     else:
     #         reactions[reaction.status_id].append(reaction)
+    # for row in parse_files.load_reactions("dataset/test_reactions.csv"):
+    #     reaction = Reaction(row[0], row[1], row[2], datetime.strptime(row[3], "%Y-%m-%d %H:%M:%S"))
+    #     if reaction.status_id not in reactions:
+    #         reactions[reaction.status_id] = [reaction]
+    #     else:
+    #         reactions[reaction.status_id].append(reaction)
     
+
     # for row in parse_files.load_comments("dataset/original_comments.csv"):
     #     comment = Comment(row[0], row[1], row[2], row[3], row[4], datetime.strptime(row[5], "%Y-%m-%d %H:%M:%S"), int(row[6]), int(row[7]))
     #     if comment.status_id not in comments:
     #         comments[comment.status_id] = [comment]
     #     else:
     #         comments[comment.status_id].append(comment)
+    # for row in parse_files.load_comments("dataset/test_comments.csv"):
+    #     comment = Comment(row[0], row[1], row[2], row[3], row[4], datetime.strptime(row[5], "%Y-%m-%d %H:%M:%S"), int(row[6]), int(row[7]))
+    #     if comment.status_id not in comments:
+    #         comments[comment.status_id] = [comment]
+    #     else:
+    #         comments[comment.status_id].append(comment)
+
+    # with open("pickles/entities.pickle", "rb") as f:
+    #     data = pickle.load(f)
+    #     users = data["users"]
+    #     friends = data["friends"]
 
     # dictionary = {"users": users, "friends": friends, "statuses": statuses, "shares": shares, "reactions": reactions, "comments": comments}
-    # with open("pickles/entities.pickle", "wb") as f:
+    # with open("pickles/test_entities.pickle", "wb") as f:
     #     pickle.dump(dictionary, f)
 
+    # trie = Trie(reduce(lambda x, y: x + y, statuses.values()))
+    # with open("pickles/test_trie.pickle", "wb") as f:
+    #     pickle.dump(trie, f)
 
-    with open("pickles/entities.pickle", "rb") as f:
+    # with open("pickles/trie.pickle", "rb") as f:
+    #     trie = pickle.load(f)
+
+
+    # with open("pickles/entities.pickle", "rb") as f:
+    #     data = pickle.load(f)
+    #     users = data["users"]
+    #     friends = data["friends"]
+    #     statuses = data["statuses"]
+    #     shares = data["shares"]
+    #     reactions = data["reactions"]
+    #     comments = data["comments"]
+
+    with open("pickles/test_entities.pickle", "rb") as f:
         data = pickle.load(f)
         users = data["users"]
         friends = data["friends"]
@@ -199,7 +247,9 @@ if __name__ == "__main__":
     # with open("pickles/friend_user_graph.pickle", "wb") as f:
     #     pickle.dump(graph, f)
 
-    with open("pickles/trie.pickle", "rb") as f:
+    # with open("pickles/trie.pickle", "rb") as f:
+    #     trie = pickle.load(f)
+    with open("pickles/test_trie.pickle", "rb") as f:
         trie = pickle.load(f)
     # trie = Trie(reduce(lambda x, y: x + y, statuses.values()))
 
